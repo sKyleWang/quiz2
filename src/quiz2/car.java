ae4aab24b26a07fdbd1a3aaf0b13ca904709723e
sKyleWang <36742284+sKyleWang@users.noreply.github.com>
2018-03-02 10:09:49 +0800

    Update car.java

--- src/quiz2/car.java
+++ src/quiz2/car.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 
 public class car {
 
-	public static void main(String[] args) {
+	public static void main(double price, double downpayment, int month, double loan, double Monthlyloan, double MonthlyCarPayment, double TotalInterest, args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("The total price of car is: ");
-		double price = s.nextDouble();
+		price = s.nextDouble();
 		System.out.println("The down payment is: ");
-		double downpayment = s.nextDouble();
+		downpayment = s.nextDouble();
 		System.out.println("The length of loan (in months)is: ");
-		int month = s.nextInt();
+		month = s.nextInt();
 		System.out.println("The interest rate for loan is: ");
-		double loan = s.nextDouble();				
+		loan = s.nextDouble();				
 		s.close();
 	
-	double Monthlyloan = loan/100/12;
-	double MonthlyCarPayment = (Monthlyloan + (Monthlyloan /
+	Monthlyloan = loan/100/12;
+	MonthlyCarPayment = (Monthlyloan + (Monthlyloan /
 			(Math.pow(1+Monthlyloan, month)-1))) * price;
-	double TotalInterest = MonthlyCarPayment * month - price;
+	TotalInterest = MonthlyCarPayment * month - price;
 	System.out.println("The monthly car payment is: "+ MonthlyCarPayment);
 	System.out.println("The total spent on interest over the term of the loan is: "+ TotalInterest);
 }
